9a7e25e0726bd5ae81a3db506adc745ce70bca40
Pavel Davydov <Davydov.works@gmail.com>
2023-09-13 11:06:33 -0400

    Add faild  @Test amountEqualsBoundary

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -44,6 +44,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    public void amountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
 
+        assertEquals(expected, actual);
+    }
 }
 
